82d32d0462f335023df61c6b3464dd749a0cafc5
T.T <t.517a3@gmail.com>
2022-11-10 14:09:08 +0900

    Add sample_project

--- sample_project.rb
+++ sample_project.rb
@@ -20,3 +20,34 @@
 #   endAt: Date;
 #   rate: number; // 稼働率
 # };
+
+require 'date'
+
+# 関数
+def labor_cost(pro, per)
+  ea = pro[:endAt]
+  sa = pro[:startAt]
+  working_days = (ea - sa).to_i
+  cost = working_days.to_i * per[:unitCost].to_i
+  puts "#{cost}円"
+end
+
+# サンプルプロジェクト
+Project = {
+  id: 1,
+  name: "string",
+  startAt: Date.new(1993, 2, 24),
+  endAt: Date.new(1993, 5, 30),
+  budget: 1000000
+}
+
+# サンプルパーソン
+Person = {
+  id: 1,
+  email: "string",
+  unitCost: 10000,
+}
+
+pro = Project
+per = Person
+labor_cost(pro, per)
